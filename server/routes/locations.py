511eb845dfe556fe88241995fd3c1c5292ec91ee
BriteyAC <BritneyArtemis@gmail.com>
2019-05-04 15:59:56 -0700

    'location_fixes'

--- server/routes/locations.py
+++ server/routes/locations.py
@@ -6,3 +6,4 @@ app.add_url_rule('/location/<location_id>/like', view_func=locations.like, endpo
 app.add_url_rule('/location/<location_id>/remove_like', view_func=locations.remove_like, endpoint='locations:remove_like', methods=['POST', 'GET'])
 
 app.add_url_rule('/add_destination', view_func=locations.add_destination, endpoint='locations:add_destination', methods=['POST', 'GET'])
+app.add_url_rule('/remove_destination/<location_id>', view_func=locations.remove_destination, endpoint='locations:remove_destination', methods=['POST', 'GET'])
