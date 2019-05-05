ddcc99b75935c3fb8b4e135cd54d0eebd2fce4f5
Matthew Wiltzius <matt.wiltzius@gmail.com>
2019-05-04 19:57:48 -0500

    updated map style

--- client/static/javascript/main.js
+++ client/static/javascript/main.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var map = L.map('map').setView([44.977753, -93.265015], 16);
     
-    L.tileLayer('https://api.mapbox.com/styles/v1/mwiltzius/cjuucw1uo1qpo1frs2vldzv8r/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/mwiltzius/cjva86tri16u31fmvziln9746/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}', {
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibXdpbHR6aXVzIiwiYSI6ImNqdXVjZTFjazBoYzY0M243OTZ1ZWx6YWMifQ.7iYMruXZbUR_2DWQpULrMg'
     }).addTo(map);
 
-    // L.Routing.control({
     //     waypoints: [
     //         L.latLng(57.75, 11.94),
     //         L.latLng(57.6792,11.949)
@@ -44,3 +43,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
 
 })
+
+// mapbox://styles/mwiltzius/cjuucw1uo1qpo1frs2vldzv8r
